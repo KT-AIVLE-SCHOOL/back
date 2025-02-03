6c2aca6437d589716ea9cf2e6605425807d54c15
jaehjoo <wnwoduq@naver.com>
2025-02-03 17:16:48 +0900

    tears

--- server_app/back/src/main/java/com/bigp/back/utils/DBCleanUp.java
+++ server_app/back/src/main/java/com/bigp/back/utils/DBCleanUp.java
@@ -1,19 +1,16 @@
 package com.bigp.back.utils;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import com.bigp.back.service.DataCleanService;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class DBCleanUp {
-    private final DataCleanService cleanService;
+    // private final DataCleanService cleanService;
 
-    @Scheduled(cron="0 0 0 * * *", zone="Asia/Seoul")
-    public void dailyDBCleanTask() {
-        cleanService.dataClean();
-    }
+    // @Scheduled(cron="0 0 0 * * *", zone="Asia/Seoul")
+    // public void dailyDBCleanTask() {
+    //     cleanService.dataClean();
+    // }
 }
