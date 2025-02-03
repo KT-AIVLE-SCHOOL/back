0a610dceb8b32ed0d229447a9f973d607aded5f7
jaehjoo <wnwoduq@naver.com>
2025-02-03 23:17:03 +0900

    Fix: hell3

--- server_app/back/src/main/java/com/bigp/back/BackApplication.java
+++ server_app/back/src/main/java/com/bigp/back/BackApplication.java
@@ -1,7 +1,10 @@
 package com.bigp.back;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -16,4 +19,16 @@ public class BackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            try {
+                jdbcTemplate.execute("SELECT 1");
+                System.out.println("Successfully connected to the database!");
+            } catch (Exception e) {
+                System.err.println("Failed to connect to the database: " + e.getMessage());
+            }
+        };
+    }
 }
